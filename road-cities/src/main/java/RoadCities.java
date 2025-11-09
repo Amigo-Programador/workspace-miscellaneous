34de4d75532fc2f7385303b2612bf4ed30f5bc1b
Amigo-Programador <amigo.programador@outlook.com>
2025-11-08 19:22:19 -0500

    2. Road & Cities. Ejercicio de programación.

--- road-cities/src/main/java/RoadCities.java
+++ road-cities/src/main/java/RoadCities.java
@@ -15,146 +15,124 @@ public class RoadCities {
     | Roads = [0, 1], [1, 2], [1, 4], [2, 3], [2, 4]       |
     |------------------------------------------------------|
   */
-  public static void main(String[] args) {
-//    List<String> citiesString = Arrays.asList("B", "A", "D", "C", "C");
-    List<Integer> cities = Arrays.asList(1, 0, 3, 2, 2);
 
-    List<int[]> roads = new ArrayList<>();
+  private static List<Integer> cities = Arrays.asList(1, 0, 3, 2, 2);
+  private static List<int[]> roads = new ArrayList<>();
+
+  private static int contador = 1;
+  private static List<List<Integer>> validRoutes = new ArrayList<>();
+  private static int[] typeVisited = new int[4]; // 0,0,0,0
+
+  public static void main(String[] args) {
+
     roads.add(new int[]{0, 1});
     roads.add(new int[]{1, 2});
     roads.add(new int[]{1, 4});
     roads.add(new int[]{2, 3});
     roads.add(new int[]{2, 4});
 
-//    int result = countValidRoutesString(citiesString, roads);
-    int result = countValidRoutes(cities, roads);
-    System.out.println("Number of valid paths: " + result);
+    findValidRoutes(cities, roads);
+
+    System.out.println("Valid routes: " + validRoutes);
 
   }
 
-  public static int countValidRoutes(List<Integer> cities, List<int[]> roads) {
-    int[] citiesVisited = new int[4];
-    int validRoutes = 0;
-    System.out.println("[init] countValidRoutes");
-    System.out.println("[cities] " + cities + " | [B, A, D, C, C]");
-    for (int i = 0; i < cities.size(); i++) {
-      Arrays.fill(citiesVisited, 0);
-      citiesVisited[cities.get(i)]++;
-      System.out.println("[for-city] i="+i + "   type: " + getTypeCity(cities.get(i))+ "   citiesVisited: "+ Arrays.toString(citiesVisited));
-      validRoutes += findRoute(cities, roads, citiesVisited, i, 2);
-      System.out.println("validRoutes: "+ validRoutes);
+  private static List<List<Integer>> findValidRoutes(List<Integer> cities, List<int[]> roads) {
+    System.out.println("[init] findValidRoutes");
+
+
+
+    for(int c=0; c<cities.size(); c++) {
+      System.out.println("[for-city="+c+"] type=" + getTypeCity(cities.get(c)));
+
+      Arrays.fill(typeVisited, 0);
+      typeVisited[cities.get(c)]++; // 0,1,0,0
+      List<Integer> validRoute = new ArrayList<>();
+      validRoute.add(c);
+      recursiveFindRoute(c, validRoute, 0);
+      System.out.println("[for-city="+c+"]");
     }
 
+    System.out.println("[end] findValidRoutes");
     return validRoutes;
   }
 
-//  public static int countValidRoutesString(List<String> cities, List<int[]> roads) {
-//    int[] citiesVisited = new int[4];
-//    int validRoutes = 0;
-//    System.out.println("[init] countValidRoutes");
-//    for (int i = 0; i < cities.size(); i++) {
-//      System.out.println("  [for] i="+i);
-//      Arrays.fill(citiesVisited, 0);
-//
-//
-//
-//      citiesVisited[getIntegerIndex(cities.get(i))]++;
-//      System.out.println("  citiesVisited: "+ Arrays.toString(citiesVisited));
-//      validRoutes += findRouteString(cities, roads, citiesVisited, i);
-//      System.out.println("  validRoutes: "+ validRoutes);
-//    }
-//
-//    return validRoutes;
-//  }
 
-  private static int findRoute(List<Integer> cities, List<int[]> roads, int[] citiesVisited, int currentCity, int depth) {
-    System.out.println(indent(depth) + "[bucle] currentCity: "+currentCity + "   type: "+ getTypeCity(cities.get(currentCity)) );
-    if (allCitiesVisited(citiesVisited)) {
-      System.out.println(indent(depth) + "*** VALID ROUTE ***");
-      return 1;
+  private static List<List<Integer>> recursiveFindRoute(Integer currentCity, List<Integer> validRoute, int depth) {
+    int nro = contador++;
+    System.out.println(indent(depth+2)+"[recursive-"+nro+"] city="+currentCity + " type="+ getTypeCity(cities.get(currentCity)) );
+
+    if(validVisitAllTypes(typeVisited)) {
+      System.out.println(indent(depth+4)+"*** ALL VISITED ***");
+      validRoutes.add(new ArrayList<>(validRoute));
+      System.out.println(indent(depth+4)+validRoutes);
+      System.out.println(indent(depth+2)+"[recursive-"+nro+"]");
+      return validRoutes;
     }
 
-    int validRoutes = 0;
-    for (int i = 0; i < roads.size(); i++) {
-      System.out.println(indent(depth) + "[for-road] i="+i + "   road: "+ Arrays.toString(roads.get(i))+ "   citiesVisited: "+ Arrays.toString(citiesVisited));
-      int[] road = roads.get(i);
-      if (road[0] == currentCity && citiesVisited[cities.get(road[1])] == 0) {
-        System.out.println(indent(depth)+"  [if] city: "+road[0] + " type: " + getTypeCity(cities.get(road[0])) +" → city: "+road[1] + " type: " + getTypeCity(cities.get(road[1])));
 
-        citiesVisited[cities.get(road[1])]++;
-        System.out.println(indent(depth)+"  update++  citiesVisited:" + Arrays.toString(citiesVisited));
+    for(int r=0; r<roads.size(); r++) {
+      System.out.println(indent(depth+4)+"[for-road-" + nro + "] " + "road="+r+" "+ Arrays.toString(roads.get(r))+ " [" + getTypeCity(cities.get(roads.get(r)[0])) +", " + getTypeCity(cities.get(roads.get(r)[1]))
+        + "] citiesVisited: "+ Arrays.toString(typeVisited) + "  validRoute: " + validRoute);
 
-        validRoutes += findRoute(cities, roads, citiesVisited, road[1], depth+4);
+      int[] road = roads.get(r);
+      if(currentCity == road[0] && typeVisited[cities.get(road[1])] == 0) {
+        System.out.println(indent(depth+6)+"[if-"+nro+"] " + getTypeCity(cities.get(road[0])) + " → " + getTypeCity(cities.get(road[1])) + " = " + getTypeCity(cities.get(road[1])) + " Not visited");
+        typeVisited[cities.get(road[1])]++;
+        validRoute.add(road[1]);
 
-        citiesVisited[cities.get(road[1])]--;
-        System.out.println(indent(depth)+"  update--  citiesVisited:" + Arrays.toString(citiesVisited));
-      } else if (road[1] == currentCity && citiesVisited[cities.get(road[0])] == 0) {
-        System.out.println(indent(depth)+"  [if] city: "+road[1] + " type: " + getTypeCity(cities.get(road[1])) +" → city: "+road[0] + " type: " + getTypeCity(cities.get(road[0])));
-        citiesVisited[cities.get(road[0])]++;
-        System.out.println(indent(depth)+"  update++  citiesVisited:" + Arrays.toString(citiesVisited));
-        validRoutes += findRoute(cities, roads, citiesVisited, road[0], depth+4);
-        citiesVisited[cities.get(road[0])]--;
-        System.out.println(indent(depth)+"  update--  citiesVisited:" + Arrays.toString(citiesVisited));
+        recursiveFindRoute(road[1], validRoute, depth+6);
+
+        typeVisited[cities.get(road[1])]--;
+        validRoute.remove(validRoute.size()-1);
+
+        System.out.println(indent(depth+6)+"typeVisited--: "+ Arrays.toString(typeVisited));
+        System.out.println(indent(depth+6)+"validRoute.remove() "+ validRoute);
+        System.out.println(indent(depth+6)+"[if-"+nro+"] ");
       }
+      if(currentCity == road[1] && typeVisited[cities.get(road[0])] == 0) {
+        System.out.println(indent(depth+6)+"[else-"+nro+"] " + getTypeCity(cities.get(road[0])) + " ← " + getTypeCity(cities.get(road[1])) + " = " + getTypeCity(cities.get(road[0])) + " Not visited");
+        typeVisited[cities.get(road[0])]++;
+        validRoute.add(road[0]);
+
+        recursiveFindRoute(road[0], validRoute, depth+6);
+
+        typeVisited[cities.get(road[0])]--;
+        validRoute.remove(validRoute.size()-1);
+
+        System.out.println(indent(depth+6)+"typeVisited--: "+ Arrays.toString(typeVisited));
+        System.out.println(indent(depth+6)+"validRoute.remove() "+ validRoute);
+        System.out.println(indent(depth+6)+"[else-"+nro+"]");
+      }
+
     }
+    System.out.println(indent(depth+2)+"[recursive-"+nro+"]");
     return validRoutes;
   }
 
-//  private static int findRouteString(List<String> cities, List<int[]> roads, int[] citiesVisited, int currentCity) {
-//    System.out.println("  [bucle] findRoute currentCity="+currentCity);
-//    if (allCitiesVisited(citiesVisited)) {
-//      return 1;
-//    }
-//
-//    int validRoutes = 0;
-//    for (int i = 0; i < roads.size(); i++) {
-//      System.out.println("    [for] i="+i + " road="+ Arrays.toString(roads.get(i)));
-//      int[] road = roads.get(i);
-//      if (road[0] == currentCity && citiesVisited[getIntegerIndex(cities.get(road[1]))] == 0) {
-//        System.out.println("    [if] road[0]="+road[0] + "  typeCity="+cities.get(road[1]));
-//        citiesVisited[getIntegerIndex(cities.get(road[1]))]++;
-//        validRoutes += findRouteString(cities, roads, citiesVisited, road[1]);
-//        citiesVisited[getIntegerIndex(cities.get(road[1]))]--;
-//      } else if (road[1] == currentCity && citiesVisited[getIntegerIndex(cities.get(road[0]))] == 0) {
-//        System.out.println("    [else] road[1]="+road[1] + "  typeCity="+cities.get(road[1]));
-//        citiesVisited[getIntegerIndex(cities.get(road[0]))]++;
-//        validRoutes += findRouteString(cities, roads, citiesVisited, road[0]);
-//        citiesVisited[getIntegerIndex(cities.get(road[0]))]--;
-//      }
-//    }
-//    return validRoutes;
-//  }
 
-  private static boolean allCitiesVisited(int[] citiesVisited) {
-    for (int count : citiesVisited) {
-      if (count == 0) {
+  private static boolean validVisitAllTypes(int[] typeVisited) {
+    for(int i : typeVisited) {
+      if(i==0) {
         return false;
       }
     }
     return true;
   }
 
-//  private static int getIntegerIndex(String i) {
-//    switch (i) {
-//      case "A": return 0;
-//      case "B": return 1;
-//      case "C": return 2;
-//      case "D": return 3;
-//      default: throw new IllegalArgumentException("Not valid index");
-//    }
-//  }
 
-  private static String getTypeCity(int i) {
-    switch (i) {
+  private static String getTypeCity(int type) {
+    switch (type) {
       case 0: return "A";
       case 1: return "B";
       case 2: return "C";
       case 3: return "D";
-      default: throw new IllegalArgumentException("Not valid index: " + i);
+      default: throw new IllegalArgumentException("Not valid type city");
     }
   }
 
   private static String indent(int depth) {
     return " ".repeat(depth);
   }
+
 }
